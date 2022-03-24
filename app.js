9a56d7a99dc88b9257bf6f1d2dc77b25f6dc92e4
Isak Ek <kjellanderek.isak@elev.ga.ntig.se>
2022-03-24 03:40:29 +0100

    final

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("hello");
+const preview = document.createElement('div')
+preview.id = 'preview'
+document.body.appendChild(preview)
+
+const images = document.querySelectorAll('img')
+images.forEach(image => {
+  image.addEventListener('click', e => {
+    preview.classList.add('active')
+    const img = document.createElement('img')
+    img.src = image.src
+    while (preview.firstChild) {
+      preview.removeChild(preview.firstChild)
+    }
+    preview.appendChild(img)
+  })
+})
+preview.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) return
+  preview.classList.remove('active')
+})
